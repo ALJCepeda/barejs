0987fdc61ee96e2323445a9d540043c29165b42e
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-05-10 01:25:37 -0700

    Adds supplant tests

--- test/index.js
+++ test/index.js
@@ -2,10 +2,25 @@ var tape = require("tape");
 var Base = require("../");
 var b = new Base();
 
-tape("is_a", function(t) {
-	t.plan(2);
+tape("supplant", function(t) {
 
+    t.equal(
+    	b.supplant("Hello {0}, My name is {1}", [ "stranger", "Bob" ]), 
+    	"Hello stranger, My name is Bob", 
+    	"String substitution using array"
+    );
 
-	t.ok(true);
-    t.equal(1+1, 2);
+    t.equal(
+    	b.supplant("A {animal}, says {sound}", { animal:"Cow", sound:"moo" }),
+    	"A Cow, says moo",
+    	"String substition with key/value pairs"
+    );
+
+    t.equal(
+    	b.supplant("The placeholder {key} remains when {substitution} fails", [ "won't", "replace" ]),
+    	"The placeholder {key} remains when {substitution} fails",
+    	"No string substitution occurred"
+    );
+
+    t.end();
 });
