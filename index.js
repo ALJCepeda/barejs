e42275191a4251644c377e953c05f81a7eda8603
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-05-12 05:45:49 +0000

    Reoganizes some code and exploses Val class

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
-/*
-	Performs a class check on each (classname, obj)
-*/
-var Base = function() {};
 
-/*
-	String introspection
-*/
-Base.prototype.supplant = function(str, values) {
-	return str.replace(/{([^{}]*)}/g, function (a, b) {
-			var r = values[b];
-			return typeof r === 'string' || typeof r === 'number' ? r : a;
-		}
-	);
+module.exports = {
+	Misc:require("./scripts/misc"),
+	Val:require("./scripts/val")
 };
-
-/*
-	Fires callback once and no more
-*/
-Base.prototype.once = function(fn, context) {
-	return function() {
-		if(fn) {
-			var result = fn.apply(context || this, arguments);
-			fn = null;
-			return result;
-		}
-	};
-};
-
-module.exports = Base;
