09c8b8bb702c9b154973496d1b54c1e955d9cdd6
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-05-13 00:17:13 -0700

    Misc methods are now static

--- scripts/misc.js
+++ scripts/misc.js
@@ -6,7 +6,7 @@ var Misc = function() {};
 /*
 	String introspection
 */
-Misc.prototype.supplant = function(str, values) {
+Misc.supplant = function(str, values) {
 	return str.replace(/{([^{}]*)}/g, function (a, b) {
 			var r = values[b];
 			return typeof r === 'string' || typeof r === 'number' ? r : a;
@@ -17,7 +17,7 @@ Misc.prototype.supplant = function(str, values) {
 /*
 	Fires callback once and no more
 */
-Misc.prototype.once = function(fn, context) {
+Misc.once = function(fn, context) {
 	return function() {
 		if(fn) {
 			var result = fn.apply(context || this, arguments);
