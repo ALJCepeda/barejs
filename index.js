80cdb87e176141dba01f9d7f1c4eb716ec259527
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-06-12 04:14:26 +0000

    Exposer not necessar with express

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-var fs = require("fs"),
-	path = require("path"),
-	Promise = require("promise"),
-	M = require("./scripts/misc"),
-	V = require("./scripts/val"),
-	O = require("./scripts/obj"),
-	E = require("./scripts/exposer");
+var Bare = require("./scripts/misc");
+Bare.Val = require("./scripts/val");
+Bare.Obj = require("./scripts/obj");
 
-var Bare = M;
-Bare.Val = V;
-Bare.Obj = O;
-Bare.Exposer = E;
 module.exports = Bare;
-
-var doBound = M.once(function(server) {
-	var expose = new E();
-	expose.listen(server);
-	return expose;
-}.bind(this));
-
-var expose = function(url, name, server) {
-	this.bound = doBound(server);
-	return this.bound.add(url, name);
-};
-
-Bare.expose = expose;
